4626e6fda55a0eab31592fb6e94455b99392b17f
Dominik Koterwa <koterwadominik@icloud.com>
2023-05-09 22:07:14 +0200

    Add Trainer class

--- package_test.R
+++ package_test.R
@@ -9,11 +9,11 @@ library(pROC)
 library(magrittr)
 library(gridExtra)
 
-setwd("/Volumes/HD/GitHub/masters/second semester/master_seminar/2023-04-17_boosting")
+setwd("/Volumes/HD/GitHub/AdvR_Project")
 
-load("medical_train_test.RData")
+load("data/medical_train_test.RData")
 
-
+medical.test$UCURNINS
 model1.formula <- UCURNINS ~ UMARSTAT + USATMED + URELATE + REGION + 
   FHOSP + FDENT + FEMER + FDOCT + UIMMSTAT + 
   UAGE + U_FTPT + U_WKSLY + U_USHRS + 
@@ -21,7 +21,6 @@ model1.formula <- UCURNINS ~ UMARSTAT + USATMED + URELATE + REGION +
   UBRACE + UEDUC3 + GENDER
 
 
-set.seed(123456789)
 params_rf <- 
   expand.grid(mtry = 15,
               # split rule
@@ -30,6 +29,7 @@ params_rf <-
               min.node.size = 250)
 
 #TRAINING AND PREDICTING
+
 
 model1 <- train(model1.formula,
                     data = medical.train[1:1000,], 
@@ -42,7 +42,6 @@ model1 <- train(model1.formula,
                     tuneGrid = params_rf,
                     trControl = trainControl(method = "none", classProbs = TRUE))
 
-model1$
 probs <- predict(model1,
                  medical.test,
                  type="prob")[, "Yes"]
@@ -59,43 +58,55 @@ medical.test["pred"] <-
 
 ### CONFUSION MATRIX ###
 
-pred <- factor(medical.test$pred)
-true <- factor(medical.test$UCURNINS)
-cm <- confusionMatrix(pred, true)
-cm$overall[["Accuracy"]]
-confusion_matrix <- as.data.frame(table(pred, true))
-confusion_matrix
-ggplot(data = confusion_matrix,
-       mapping = aes(x = pred,
-                     y = true)) +
-  geom_tile(aes(fill = Freq)) +
-  geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
-  scale_fill_gradient(low = "#655DBB",
-                      high = "#ECF2FF") +
-  theme_bw() +
-  theme(panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        plot.title = element_text(hjust=0.5)) +
-  labs(title = "Confusion Matrix",
-       x = "Predicted class",
-       y = "True class")
+plot_confusion_matrix <- function(data, predicted, label_column){
 
+  pred <- factor(predicted)
+  true <- factor(data[[label_column]])
+  
+  confusion_matrix <- as.data.frame(table(pred, true))
+  
+  p <- ggplot(data = confusion_matrix,
+          mapping = aes(x = pred,
+                        y = true)) +
+      geom_tile(aes(fill = Freq)) +
+      geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+      scale_fill_gradient(low = "#655DBB",
+                          high = "#ECF2FF") +
+      theme_bw() +
+      theme(panel.border = element_blank(),
+            panel.grid.major = element_blank(),
+            plot.title = element_text(hjust=0.5)) +
+      labs(title = "Confusion Matrix",
+          x = "Predicted class",
+          y = "True class")
+  return(p)
+}
+medical.test$pred
+medical.test[, UCURNINS]
+
+p <- plot_confusion_matrix(medical.test, medical.test$pred, "UCURNINS" )
+p
 ### AUROC ###
 
-labels <- (medical.test[, "UCURNINS"]) %>% pull
-pred <- probs
+plot_auroc <- function(test_data, label_column, predicted_probs){
+  labels <- (test_data[, label_column]) %>% pull
+  pred <- predicted_probs
+  
+  thresholds <- seq(0, 1, by = 0.1)
+  roc_data <- roc(predictor=pred, response=labels, thresholds=thresholds)
+  
+  p <-plot(roc_data,
+        print.auc=TRUE,
+        auc.polygon=TRUE,
+        legacy.axes=TRUE,
+        main="AUROC Curve",
+        xlab="False Positive Rate",
+        ylab="True Positive Rate")
+  
+  return(p)
+}
 
-thresholds <- seq(0, 1, by = 0.1)
-roc_data <- roc(predictor=probs, response=labels, thresholds=thresholds)
-roc_data$thresholds
-
-plot(roc_data,
-     print.auc=TRUE,
-     auc.polygon=TRUE,
-     legacy.axes=TRUE,
-     main="AUROC Curve",
-     xlab="False Positive Rate",
-     ylab="True Positive Rate")
+p<-plot_auroc(medical.test, "UCURNINS", probs)
 
 ### METRICS ###
 
@@ -110,11 +121,11 @@ calc_metrics <- function(actual, predicted) {
   return(list(accuracy = accuracy, recall = recall, precision = precision, f1=f1))
 }
 
-calc_balanced_metrics <- function(actual, predicted){
-  tp <- sum(actual == "Yes" & predicted == "Yes")
-  tn <- sum(actual == "No" & predicted == "No")
-  fp <- sum(actual == "No" & predicted == "Yes")
-  fn <- sum(actual == "Yes" & predicted == "No")
+calc_balanced_metrics <- function(labels, predicted){
+  tp <- sum(labels == "Yes" & predicted == "Yes")
+  tn <- sum(labels == "No" & predicted == "No")
+  fp <- sum(labels == "No" & predicted == "Yes")
+  fn <- sum(labels == "Yes" & predicted == "No")
   
   precision_pos <- tp / (tp+fp)
   recall_pos <- tp / (tp+fn)
@@ -139,7 +150,7 @@ calc_balanced_metrics <- function(actual, predicted){
               balanced_f1 = balanced_f1))
 }
 
-metrics <- calc_balanced_metrics(true, pred)
+metrics <- calc_balanced_metrics(medical.test$UCURNINS, medical.test$pred)
 metrics
 
 ### LEARNING CURVE ###
@@ -209,7 +220,7 @@ calculate_learning_curve <- function(train_data, test_data, formula, tune_grid) 
 result <- calculate_learning_curve(medical.train, medical.test, model1.formula, params_rf)
 
 
-plot_acc <- function(metrics, train_sizes = seq(0.01, 0.9, by = 0.05)){
+plot_metrics <- function(metrics, train_sizes = seq(0.01, 0.9, by = 0.05)){
   
   metrics_df <- as.data.frame(do.call(cbind, metrics))
   metrics_df <- metrics_df %>% mutate_all(as.numeric)
@@ -249,7 +260,7 @@ plot_acc <- function(metrics, train_sizes = seq(0.01, 0.9, by = 0.05)){
    
   return (list(acc =p_acc, prec = p_prec, rec=p_rec, f1=p_f1))  
   }
-plots <- plot_acc(result)
+plots <- plot_metrics(result)
 plots
 grid.arrange(plots$acc, plots$prec, plots$rec, plots$f1, ncol = 2)
 
@@ -271,6 +282,295 @@ plot_probs <- function(probs, labels){
 }
 
 
+################################################################################################################################################
+##########################################################CLASS PREPARATION###############################################################
+################################################################################################################################################
 
+library(R6)
 
+Trainer <- R6Class(
+  "Trainer",
+  
+  public = list(
 
+    # initialize method to create a new instance of the Trainer class
+  
+    train_data = NULL,
+    test_data = NULL,
+    label_column = NULL,
+    labels = NULL,
+    formula = NULL,
+    method = NULL,
+    num_trees = NULL,
+    importance = NULL,
+    tune_grid = NULL,
+    num_threads = NULL,
+    
+    initialize = function(train_data, 
+                          test_data,
+                          label_column) 
+      {
+
+      self$train_data <- train_data
+      self$test_data <- test_data
+      self$label_column <- label_column
+      self$labels <- self$test_data[[label_column]]
+    },
+    
+    data_head = function(df){
+      head(df)
+    },
+    
+    create_random_forest = function(train_data,
+                                    test_data,
+                                    formula,
+                                    method,
+                                    num_trees,
+                                    importance,
+                                    tune_grid,
+                                    num_threads)
+      {
+      self$formula = formula
+      self$num_trees = num_trees
+      self$tune_grid = tune_grid
+      self$method = method
+      self$importance = importance
+      self$num_threads = num_threads
+      
+      model <- train(formula,
+                      data = train_data[1:1000,], 
+                      method = "ranger", 
+                      num.trees = num_trees, # default 500
+                      # how many cores/threads used
+                      num.threads = num_threads,
+                      # which importance measure
+                      importance = importance,
+                      tuneGrid = tune_grid,
+                      trControl = trainControl(method = "none", classProbs = TRUE))
+      
+      probs <- predict(model,
+                       test_data,
+                       type="prob")[, "Yes"]
+      
+      predicted <-predict(model1,
+                          medical.test %>% 
+                            select(UMARSTAT, USATMED, URELATE, REGION, 
+                                   FHOSP, FDENT, FEMER, FDOCT, UIMMSTAT, 
+                                   UAGE, U_FTPT, U_WKSLY, U_USHRS, 
+                                   HOTHVAL, HRETVAL, HSSVAL, 
+                                   UBRACE, UEDUC3, GENDER))
+      
+      return(list(model=model, probs=probs, predicted=predicted))
+    },
+    
+    plot_confusion_matrix = function(data, 
+                                      predicted, 
+                                      labels)
+      {
+      
+      confusion_matrix <- as.data.frame(table(predicted, labels))
+      p <- ggplot(data = confusion_matrix,
+                  mapping = aes(x = predicted,
+                                y = labels)) +
+        geom_tile(aes(fill = Freq)) +
+        geom_text(aes(label = sprintf("%1.0f", Freq)), vjust = 1) +
+        scale_fill_gradient(low = "#655DBB",
+                            high = "#ECF2FF") +
+        theme_bw() +
+        theme(panel.border = element_blank(),
+              panel.grid.major = element_blank(),
+              plot.title = element_text(hjust=0.5)) +
+        labs(title = "Confusion Matrix",
+             x = "Predicted class",
+             y = "True class")
+      
+      return(p)
+    },
+    
+    calc_balanced_metrics = function(labels, predicted)
+      {
+      tp <- sum(labels == "Yes" & predicted == "Yes")
+      tn <- sum(labels == "No" & predicted == "No")
+      fp <- sum(labels == "No" & predicted == "Yes")
+      fn <- sum(labels == "Yes" & predicted == "No")
+      
+      precision_pos <- tp / (tp+fp)
+      recall_pos <- tp / (tp+fn)
+      f1_pos <- 2 * precision_pos * recall_pos / (precision_pos + recall_pos)
+      
+      precision_neg <- tn / (tn+fn)
+      recall_neg <- tn / (tn+fp)
+      f1_neg <- 2 * precision_neg * recall_neg / (precision_neg + recall_neg)
+      
+      balanced_precision <- (precision_pos + precision_neg) / 2
+      balanced_recall <- (recall_pos + recall_neg) / 2
+      balanced_f1 <- (f1_pos + f1_neg) / 2
+      
+      return(list(precision_pos = precision_pos,
+                  recall_pos = recall_pos,
+                  f1_pos = f1_pos,
+                  precision_neg = precision_neg,
+                  recall_neg = recall_neg,
+                  f1_neg = f1_neg,
+                  balanced_precision = balanced_precision,
+                  balanced_recall = balanced_recall,
+                  balanced_f1 = balanced_f1))
+    },
+    
+    calc_metrics = function(labels, predicted) 
+      {
+      
+      cm <- confusionMatrix(predicted, labels)
+      accuracy <- cm$overall[["Accuracy"]]
+      recall <- cm$byClass[["Recall"]]
+      precision <- cm$byClass[["Precision"]]
+      f1 <- cm$byClass[["F1"]]
+      
+      return(list(accuracy = accuracy, recall = recall, precision = precision, f1=f1))
+    },
+    
+    calculate_learning_curve = function(train_data, test_data, label_column) 
+      {
+      
+      train_sizes <- seq(0.01, 0.9, by = 0.05)
+      metrics <- list(train_acc = list(), 
+                      train_prec = list(), 
+                      train_rec = list(), 
+                      train_f1 = list(),
+                      test_acc = list(), 
+                      test_prec = list(), 
+                      test_rec = list(), 
+                      test_f1 = list())
+      
+      for (i in seq_along(train_sizes)) {
+        train_idx <- sample.int(nrow(train_data), size=floor(train_sizes[i] * nrow(train_data)), replace = FALSE)
+        training_data <- train_data[train_idx, ]
+        
+        print(nrow(training_data))
+        model <- train(self$formula,
+                      data = train_data[1:1000,], 
+                      method = self$method, 
+                      num.trees = self$num_trees, # default 500
+                      # how many cores/threads used
+                      num.threads = self$num_threads,
+                      # which importance measure
+                      importance = self$importance,
+                      tuneGrid = self$tune_grid,
+                      trControl = trainControl(method = "none", classProbs = TRUE))
+        
+        train_preds <- predict(model,
+                               train_data %>% 
+                                 select(UMARSTAT, USATMED, URELATE, REGION, 
+                                        FHOSP, FDENT, FEMER, FDOCT, UIMMSTAT, 
+                                        UAGE, U_FTPT, U_WKSLY, U_USHRS, 
+                                        HOTHVAL, HRETVAL, HSSVAL, 
+                                        UBRACE, UEDUC3, GENDER))
+        
+        test_preds <- predict(model,
+                              test_data %>% 
+                                select(UMARSTAT, USATMED, URELATE, REGION, 
+                                       FHOSP, FDENT, FEMER, FDOCT, UIMMSTAT, 
+                                       UAGE, U_FTPT, U_WKSLY, U_USHRS, 
+                                       HOTHVAL, HRETVAL, HSSVAL, 
+                                       UBRACE, UEDUC3, GENDER))
+        
+        
+        training_metrics <- calc_metrics(train_data[[label_column]], train_preds)
+        testing_metrics <- calc_metrics(test_data[[label_column]], test_preds)
+        
+        metrics$train_acc[[i]] <- training_metrics$accuracy
+        metrics$train_prec[[i]] <- training_metrics$precision
+        metrics$train_rec[[i]] <- training_metrics$recall
+        metrics$train_f1[[i]] <- training_metrics$f1
+        
+        metrics$test_acc[[i]] <- testing_metrics$accuracy
+        metrics$test_prec[[i]] <- testing_metrics$precision
+        metrics$test_rec[[i]] <- testing_metrics$recall
+        metrics$test_f1[[i]] <- testing_metrics$f1
+        
+      }
+      return(metrics)
+    },
+    plot_metrics = function(metrics, train_sizes = seq(0.01, 0.9, by = 0.05))
+      {
+      
+      metrics_df <- as.data.frame(do.call(cbind, metrics))
+      metrics_df <- metrics_df %>% mutate_all(as.numeric)
+      
+      p_acc <-  ggplot(metrics_df, aes(x=train_sizes)) +
+        geom_line(aes(y=train_acc, color="train_acc")) +
+        geom_line(aes(y=test_acc, color="test_acc")) +
+        scale_color_manual(values = c("train_acc"="blue", "test_acc"="red"),
+                           guide = guide_legend(reverse = TRUE)) +
+        xlab("Fraction of training data") +
+        ylab("Accuracy") 
+      
+      p_prec <-  ggplot(metrics_df, aes(x=train_sizes)) +
+        geom_line(aes(y=train_prec, color="train_prec")) +
+        geom_line(aes(y=test_prec, color="test_prec")) +
+        scale_color_manual(values = c("train_prec"="blue", "test_prec"="red"),
+                           guide = guide_legend(reverse = TRUE)) +
+        xlab("Fraction of training data") +
+        ylab("Preicision") 
+      
+      p_rec <-  ggplot(metrics_df, aes(x=train_sizes)) +
+        geom_line(aes(y=train_rec, color="train_rec")) +
+        geom_line(aes(y=test_rec, color="test_rec")) +
+        scale_color_manual(values = c("train_rec"="blue", "test_rec"="red"),
+                           guide = guide_legend(reverse = TRUE)) +
+        xlab("Fraction of training data") +
+        ylab("Recall") 
+      
+      p_f1 <-  ggplot(metrics_df, aes(x=train_sizes)) +
+        geom_line(aes(y=train_f1, color="train_f1")) +
+        geom_line(aes(y=test_f1, color="test_f1")) +
+        scale_color_manual(values = c("train_f1"="blue", "test_f1"="red"),
+                           guide = guide_legend(reverse = TRUE)) +
+        xlab("Fraction of training data") +
+        ylab("F1") 
+      
+      
+      return (list(acc =p_acc, prec = p_prec, rec=p_rec, f1=p_f1))  
+    },
+    
+    plot_probs = function(probs, labels)
+      {
+      df_with_probs <- data.frame(probs = probs, label = labels)
+      p <- ggplot(df_with_probs, aes(x=probs, fill=label)) +
+        geom_histogram(position="stack", bins=30, breaks=seq(from=0.05, to=1, by=0.05)) +
+        scale_x_continuous(limits=c(0.0, 1.0),
+                           expand=expansion(mult=0,
+                                            add=0)) +
+        scale_y_continuous(expand=expansion(mult=0,
+                                            add=0))
+      
+      
+      labs(x="Predicted probability", y="Count")
+      return(p)
+    }
+    
+  )
+)
+
+my_trainer <- Trainer$new(medical.train, medical.test, "UCURNINS")
+
+my_trainer$data_head(my_trainer$train_data)
+results <- my_trainer$create_random_forest(my_trainer$train_data, 
+                                my_trainer$test_data,
+                                formula = model1.formula,
+                                method = "ranger",
+                                num_trees = 100,
+                                importance = "impurity",
+                                tune_grid = params_rf,
+                                num_threads = 4)
+
+pred <- results$predicted
+my_trainer$plot_confusion_matrix(my_trainer$test_data, pred, my_trainer$labels)
+metrics_specific <- my_trainer$calc_balanced_metrics(my_trainer$labels, pred)
+metrics_simple <- my_trainer$calc_metrics(my_trainer$labels, pred)
+
+learning_curve <- my_trainer$calculate_learning_curve(my_trainer$train_data, my_trainer$test_data, my_trainer$label_column)
+my_trainer$plot_metrics(learning_curve)
+my_trainer$plot_probs(results$probs, my_trainer$labels)
+
+metrics_specific
