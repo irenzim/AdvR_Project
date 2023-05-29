2be7bc821ce9933950ecb05f496e9436f4a917e5
Pearly <55735687+theabandonedpluto@users.noreply.github.com>
2023-05-29 22:18:04 +0300

    Delete FunctionCpp.R

--- FunctionCpp.R
+++
@@ -1,8 +0,0 @@
-sourceCpp("DfFunctions.cpp")
-
-setwd("C:/Users/pearly/Desktop/University Materials/02. summer semester 2023/Adv.R/projects_training metric/Final Submission")
-load("medical_train_test.RData")
-
-summary_df(medical.train)
-print(columnNames(medical.train))
-print(df_dim(medical.train))
